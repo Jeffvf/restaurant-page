ac333f42756ba5cadc7659ef72cff3117752aa9b
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-09-19 09:23:55 -0300

    feat: headline

--- src/page-loading.js
+++ src/page-loading.js
@@ -7,10 +7,19 @@ function addImage(element) {
     element.appendChild(image);
 }
 
+function addHeadline(element){
+    const headline = document.createElement('h1');
+    headline.textContent = 'Wonderful restaurant';
+    headline.style.color = 'white';
+
+    element.appendChild(headline);
+}
+
 function loadElements(){
     const content = document.getElementById('content');
     console.log(content);
     addImage(content);
+    addHeadline(content);
 }
 
 export {loadElements}
