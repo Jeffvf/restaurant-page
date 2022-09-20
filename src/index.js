cfbcee50ea0f857774f3c6782fd3abd09b2901b8
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-09-20 19:56:06 -0300

    refactor: page loading file to module

--- src/index.js
+++ src/index.js
@@ -2,79 +2,80 @@ import {loadHome} from './home-loading.js'
 import {menu} from './menu-loading.js'
 import css from './styles.css'
 
-const content = document.getElementById('content');
+const MainPage = (() => {
+    const content = document.getElementById('content');
 
-function createHeader(){
-    const header = document.createElement('header');
+    function createHeader(){
+        const header = document.createElement('header');
 
-    header.classList.add('header');
+        header.classList.add('header');
+        content.appendChild(header);
 
-    content.appendChild(header);
-
-    return header;
-}
-
-function createTitle(header){
-    const title = document.createElement('h1');
-
-    title.textContent = 'Le Festin';
-
-    title.classList.add('title');
-
-    header.appendChild(title);
-}
-
-function createTab(header, name){
-    const tab = document.createElement('div');
-
-    tab.classList.add('tab');
-
-    tab.textContent = name;
-
-    header.appendChild(tab);
-}
-
-function loadModule(tab){
-    const tabName = tab.innerText;
-
-    if(content.children.length > 1){
-        content.removeChild(content.lastChild);
+        return header;
     }
 
-    if(tabName == 'Home'){
-        content.appendChild(loadHome());
-    }
-    else if(tabName == 'Menu'){
-        content.appendChild(menu.showMenu());
-    }
-    else{
+    function createTitle(header){
+        const title = document.createElement('h1');
 
+        title.textContent = 'Le Festin';
+        title.classList.add('title');
+
+        header.appendChild(title);
     }
-}
 
-function tabbedBrowsing(){
-    const tabs = document.getElementsByClassName('tab');
+    function createTab(header, name){
+        const tab = document.createElement('div');
 
-    Array.from(tabs).forEach(tab => {
-        tab.addEventListener('click', () => {
-            loadModule(tab);
+        tab.classList.add('tab');
+        tab.textContent = name;
+
+        header.appendChild(tab);
+    }
+
+    function loadModule(tab){
+        const tabName = tab.innerText;
+
+        if(content.children.length > 1){
+            content.removeChild(content.lastChild);
+        }
+
+        if(tabName == 'Home'){
+            content.appendChild(loadHome());
+        }
+        else if(tabName == 'Menu'){
+            content.appendChild(menu.showMenu());
+        }
+        else{
+
+        }
+    }
+
+    function tabbedBrowsing(){
+        const tabs = document.getElementsByClassName('tab');
+
+        Array.from(tabs).forEach(tab => {
+            tab.addEventListener('click', () => {
+                loadModule(tab);
+            });
         });
-    });
-}
+    }
 
-function loadPage(){
-    const header = createHeader();
+    function loadPage(){
+        const header = createHeader();
 
-    createTitle(header);
+        createTitle(header);
 
-    createTab(header, 'Home');
-    createTab(header, 'Menu');
-    createTab(header, 'Info');
+        createTab(header, 'Home');
+        createTab(header, 'Menu');
+        createTab(header, 'Info');
 
-    content.appendChild(loadHome());
+        content.appendChild(loadHome());
 
-    tabbedBrowsing();
-}
+        tabbedBrowsing();
+    }
 
-loadPage();
+    return {loadPage};
+})();
+
+MainPage.loadPage();
 
